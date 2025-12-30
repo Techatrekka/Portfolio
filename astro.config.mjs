2a6a63f97f51b240a65269cfa019e8150bc7f6e3
Sean <saslynch12@gmail.com>
2025-12-30 11:46:58 +0000

    PackageUpdate

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://techatrekka.github.io',
-  //base: 'Portfolio',
+  base: '/',
   devToolbar: {
     enabled: false
   },
