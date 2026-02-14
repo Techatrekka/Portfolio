b8e3055824af06f7ecf17e3257ff31cf9f247c78
Sean <saslynch12@gmail.com>
2026-02-14 00:09:47 +0000

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://techatrekka.github.io/Portfolio/',
-	base: '/',
+	site: 'https://techatrekka.github.io/',
+	base: '/Portfolio/',
+	publicDir: '/public/',
 	devToolbar: {
 		enabled: false,
 	},
